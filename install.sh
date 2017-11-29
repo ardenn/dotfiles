33fded919ab5f92cccdbecfa24dbd900ed39b18d
ardenn <mc_alila@outlook.com>
2017-11-29 13:24:07 +0000

    ..

--- install.sh
+++ install.sh
@@ -3,8 +3,9 @@
 # Create symlinks in home folder to dotfiles
 for dotfile in `find $HOME/dotfiles/dots`;
 do
+    item = $(basename $dotfile)
     echo "Creating symbolic link for $dotfile"
-    ln -sf $HOME/dotfiles/dots $HOME/$(basename $dotfile)
+    ln -sf $HOME/dotfiles/dots/$item $HOME/$item
 done
 
 #Copy files to root home folder
