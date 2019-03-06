0987ae207626df92f754f615a52329dab8df5fd3
ardenn <mc_alila@outlook.com>
2019-03-06 10:52:33 +0300

    Cleaned installed_apps.sh

--- install_apps.sh
+++ install_apps.sh
@@ -1,41 +1,47 @@
-#!/bin/bash
-# echo "Setting up My Ubuntu ..."
+#!/bin/sh
+YELLOW='\033[1;33m'
+NC='\033[0m'
 
-# echo "  Updating the system ..."
-# sudo apt update && sudo apt upgrade
+echo "Setting up My Ubuntu ..."
+
+echo "  Updating the system ..."
+sudo apt update && sudo apt upgrade
 
 # echo "  Installing Ubuntu Restricted Extras ..."
-# sudo apt install ubuntu-restricted-extras
+sudo apt install ubuntu-restricted-extras
 
 # echo "  Installing git"
-# sudo apt install git-all
+# sudo apt install git
 
-# echo "  Downloading dotfiles"
-# git clone git@github.com:ardenn/dotfiles.git
+echo "  Downloading dotfiles"
+git clone git@github.com:ardenn/dotfiles.git
 
 
 echo "Begin Installing Apps..."
 
 echo "1. Installing ZSH..."
-#sudo apt install zsh
+sudo apt install zsh
+
 echo "      Switching to ZSH..."
-#chsh -s $(which zsh)
+chsh -s $(which zsh)
 
 echo "2. Installing oh-my-zsh"
-#sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
 
-apps=("tlp","tlp-rdw","gnome-tweak-tool","postgresql","postgresql-contrib","audacious","htop","timeshift","zeal","kazam","gufw")
-COUNTER=3
-for i in "${apps[@]}"
-do
-   echo "$COUNTER: Installing $i ..."
-   sudo apt install -y $i
-   COUNTER=$[COUNTER + 1]
-done
+apps="tlp tlp-rdw gnome-tweak-tool postgresql postgresql-contrib htop zeal kazam gufw python3-pip curl chrome-gnome-shell"
+echo "$COUNTER. Installing $apps"
+sudo apt install $apps
 
 echo "$COUNTER. Installing virtualenv"
 pip3 install virtualenv
 COUNTER=$[COUNTER + 1]
+
+echo "$COUNTER. Installing Uget and Uget-Integrator"
+sudo add-apt-repository ppa:plushuang-tw/uget-stable
+sudo apt install uget aria2
+
+sudo add-apt-repository ppa:uget-team/ppa
+sudo apt install uget-integrator
 
 echo "$COUNTER. Installing Etcher"
 echo "deb https://deb.etcher.io stable etcher" | sudo tee /etc/apt/sources.list.d/balena-etcher.list
@@ -66,5 +72,9 @@ curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
 sudo apt update
 sudo apt install -y yarn
+COUNTER=$[COUNTER + 1]
+
+echo "$COUNTER. Installing Communitheme"
+snap install communitheme
 
 echo "Done"
