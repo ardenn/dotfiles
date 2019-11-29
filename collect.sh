65540d22e934b20c435d6460409b060cdf6e263a
Rodgers Ouma <mc_alila@outlook.com>
2019-11-29 15:46:31 +0300

    Added color to collect.sh

--- collect.sh
+++ collect.sh
@@ -1,6 +1,13 @@
 #!/bin/zsh
 #moves selected files to newly created .dotfile/dots folder and creates symbolic
 
+RED="\033[31m"
+GREEN="\033[32m"
+YELLOW="\033[33m"
+BLUE="\033[34m"
+BOLD="\033[1m"
+RESET="\033[m"
+
 # STEPS:.
 # 1. create and array of with .dotfiles elements.
 # 2. create a new directory .dotfiles if this directory does not exits.
@@ -13,39 +20,39 @@ local -a Dotfiles
 Dotfiles=("bash_aliases" "bashrc" "gitconfig" "gitignore_global" "zshrc")
 
 # inform the user and print the whole array on the screen:
-echo 'Going to move the following selected .dotfiles:'
+echo "\n${YELLOW}Going to move the following selected dotfiles:${RESET}\n"
 for item in ${Dotfiles[@]};
 do
-    echo $HOME/.$item
+    echo "\t- ${BLUE}${HOME}/.${item}${RESET}"
 done
 while true
 do
-    echo "Do you still wish to continue? Yes [y] No [n]"
+    echo "\n${YELLOW}Do you still wish to continue? Yes [y] No [n]${RESET}"
     read "reply?Response: "
     if [[ $reply == "y" ]];
     then
     	# 2. create a variable for new directory for storing dotfiles..
 		DIR=~/dotfiles
-		cd ~ && mkdir -p $DIR && mkdir -p $DIR/dots
-		echo 'Directory created!'
+		cd ~ && mkdir -p $DIR/dots
+		echo "${GREEN}Directory created!${RESET}\n"
 			 
 		# move selected dotfiles to new directory .dotfiles
 		for dotfile in ${Dotfiles[@]};
 		do
             if [ -e ~/.$dotfile ] && ! [ -L ~/.$dotfile ]
             then
-                echo "Moving $dotfile into directory $DIR/dots"
+                echo "${BLUE}\t - Moving $dotfile into directory $DIR/dots${RESET}"
                 mv ~/.$dotfile $DIR/dots
             fi
-			echo "Creating symbolic for $dotfile"
+			echo "\t- ${BLUE}Creating symbolic for $dotfile${RESET}"
 			ln -sf $DIR/dots/.$dotfile ~/.$dotfile
 		done
         break
     elif [[ $reply == "n" ]];
     then
-            echo "Exiting..."
+            echo "${GREEN}Exiting...${RESET}"
             break
     else
-            echo "Invalid response, try again!!"
+            echo "${RED}Invalid response, try again!!${RESET}"
     fi
 done
